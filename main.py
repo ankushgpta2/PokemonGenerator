99bab875a720bbd401367b06ea0070aae41adedf
ankushgpta2 <ankushgpta2@gmail.com>
2024-10-03 17:37:58 -0400

    getting the image names from the filenames

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 import re
 
 
-# store cleaned name for each pokemon 
+# Store cleaned name for each pokemon 
 df = pd.read_csv('data/Pokedex_Ver_SV2.csv')
 df["Cleaned_Name"] = df["Original_Name"].apply(lambda x: re.sub(r'[^A-Za-z0-9]', '', x.strip().lower()))
+
+# Getting the image names from the filenames 
+directory = "data/pokemon_images"
+image_names = [os.path.splitext(file)[0].strip().lower() for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
