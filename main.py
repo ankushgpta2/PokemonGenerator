ce9e70fd52c684f105945d48193ebf9a0794b632
ankushgpta2 <ankushgpta2@gmail.com>
2024-10-09 10:54:58 -0400

    data loader class initialization and method call now incorporated into main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from src.processing.load_data import DataLoader
 
 def main():
     # Retrieve the data and save it in proper location (if not present)
-    DataLoader.load_the_data()
+    loader = DataLoader()
+    loader.load_the_data()
 
 if __name__ == "__main__":
     main()
